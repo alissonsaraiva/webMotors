5fd88ea4686265493e1e8554b84df2f36312139e
alisson saraiva <alissonsaraiva@gmail.com>
2022-06-29 11:00:35 -0300

    implementação do metodo wait_page_load

--- features/step_definitions/web/web_webmotors.rb
+++ features/step_definitions/web/web_webmotors.rb
@@ -1,5 +1,6 @@
 Dado('que desejo pesquisar um veículo por modelo') do
     home_webmotors.load
+    wait_page_load
 end
   
 Quando('seleciono o tipo de veículo carro') do
@@ -12,12 +13,13 @@ Quando('busco pelo modelo do carro') do
 end
   
 Então('o sistema apresenta a lista de veículos') do
-    home_webmotors.displayed?
+    wait_page_load
     expect(home_webmotors.has_lista_veiculos_encontrados?).to eq true    
 end
   
 Dado('que desejo visualizar o contato do vendedor num determinado anúncio') do
     home_webmotors.load
+    wait_page_load
     home_webmotors.clicar_ver_ofertas
 end
   
@@ -29,12 +31,14 @@ end
 Então('visualizo o contato do vendedor') do
     new_window = windows.last
     page.within_window new_window do
+        wait_page_load
         expect(detalhe_veiculo.has_contato_vendedor?).to eq true
     end
 end
   
 Dado('que desejo visualizar o preço do veículo num determinado anúncio') do
     home_webmotors.load
+    wait_page_load
     home_webmotors.clicar_ver_ofertas
 end
 
@@ -45,6 +49,7 @@ end
 Então('visualizo o preço do veículo') do
     new_window = windows.last
     page.within_window new_window do
+        wait_page_load
         expect(detalhe_veiculo.has_preco_veiculo?).to eq true
     end
 end
--- features/support/page_object.rb
+++ features/support/page_object.rb
@@ -16,6 +16,18 @@ module PageObject
       DetalheVeiculo.new
     end
 
+    def wait_page_load(count=5)
+      init = 0
+      while(init <= count)
+        if(page.execute_script("return document.readyState").eql?('complete'))
+          break
+        elsif init == 5
+          raise ArgumentError.new("Page not loaded fully")
+        end
+        init += 1
+      end
+    end
+
 end
 
   
